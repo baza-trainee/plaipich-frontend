4a9abb529146b282829653ed70ac43304a2632fb
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-10-16 16:36:07 +0300

    Added base structure

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer>
+            <hr />
+            <div className="italic">Footer</div>
+            <hr />
+        </footer>
+    )
+}
