a9fc96b8151e1f3510ba0676251d8f6175466ac2
Skala10 <skalamelitopol10@gmail.com>
2023-11-07 18:17:10 +0100

    fix: small update

--- src/components/section1/section1.tsx
+++ src/components/section1/section1.tsx
@@ -1,5 +1,3 @@
-import Button from "../button/Button"
-
 export default function Section1() {
   return (
     <section>
@@ -10,10 +8,6 @@ export default function Section1() {
         mollitia at, nam explicabo ab ut maiores molestias cumque deleniti!
         Iste, deleniti esse?
       </p>
-      <Button type="primary">Primary</Button>
-      <Button type="secondary">secondary</Button>
-      <Button type="orange">orange</Button>
-      <Button type="transparent">orange</Button>
     </section>
   )
 }
