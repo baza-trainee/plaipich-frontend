50d5146e6e63722513886994f92d008f838353db
Skala10 <skalamelitopol10@gmail.com>
2023-11-07 13:07:19 +0100

    feat:made butttons without dark theme

--- src/components/section1/section1.tsx
+++ src/components/section1/section1.tsx
@@ -1,8 +1,19 @@
+import Button from "../button/Button"
+
 export default function Section1() {
-    return (
-        <section>
-            <h1 className="text-3xl">Section 1</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus architecto expedita magnam inventore tempore labore aliquid neque mollitia at, nam explicabo ab ut maiores molestias cumque deleniti! Iste, deleniti esse?</p>
-        </section>
-    )
+  return (
+    <section>
+      <h1 className="text-3xl dark">Section 1</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+        architecto expedita magnam inventore tempore labore aliquid neque
+        mollitia at, nam explicabo ab ut maiores molestias cumque deleniti!
+        Iste, deleniti esse?
+      </p>
+      <Button type="primary">Primary</Button>
+      <Button type="secondary">secondary</Button>
+      <Button type="orange">orange</Button>
+      <Button type="transparent">orange</Button>
+    </section>
+  )
 }
