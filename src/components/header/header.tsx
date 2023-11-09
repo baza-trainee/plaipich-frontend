c7d8219835e9b145bbc089dde4180fc769a412cb
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-11-09 14:01:47 +0200

    Feat: Added components and reexport

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-export default function Header() {
+export  const Header = () => {
     return (
         <header>
             <hr />
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { Carousel } from './carousel/carousel';
+export { Contacts } from './contacts/contacts';
+export { Footer } from './footer/footer';
+export { Header } from './header/header';
