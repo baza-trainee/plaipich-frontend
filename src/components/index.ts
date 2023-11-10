3b325140be852b796e833fc3ac058d24da0db26a
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-11-10 09:23:34 +0200

    fix: put not found to components folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Carousel } from './carousel/carousel';
 export { Contacts } from './contacts/contacts';
 export { Footer } from './footer/footer';
 export { Header } from './header/header';
+export { NotFound } from './not-found/not-found'
--- src/components/not-found/not-found.tsx
+++ src/components/not-found/not-found.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-const NotFound = () => {
+export const NotFound = () => {
   return (
     <div>NotFound</div>
   )
 }
-
-export default NotFound
