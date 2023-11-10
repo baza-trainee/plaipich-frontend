cd3726979067d6f206c1690e0729de69db37c29c
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-11-10 19:41:45 +0200

    feat: added all components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { Carousel } from './carousel/carousel';
 export { Contacts } from './contacts/contacts';
+export { Footer } from './footer/footer';
+export { Header } from './header/header';
 export { NotFound } from './not-found/not-found'
