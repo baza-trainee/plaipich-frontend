6a2dc00454b906d306bcab6431a9581559053709
alexxuss1996 <alekseymelnych132@gmail.com>
2023-11-10 20:55:54 +0200

    fix: place NotFound in the root route

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
-export { Carousel } from './carousel/carousel';
-export { Contacts } from './contacts/contacts';
-export { Footer } from './footer/footer';
-export { Header } from './header/header';
-export { NotFound } from './not-found/not-found'
+export { Carousel } from "./carousel/carousel";
+export { Contacts } from "./contacts/contacts";
+export { Footer } from "./footer/footer";
+export { Header } from "./header/header";
