fb5f9a1517900dd8e3d3d141f05a5707c2531293
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-11-10 19:59:02 +0200

    feat: added components

---
+++ src/app/layout.tsx
@@ -0,0 +1,54 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import React from "react";
+
+import { Footer,Header } from "@/components";
+
+const fixel = localFont({
+  src: [
+    {
+      path: "./fonts/FixelDisplay-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/FixelDisplay-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/FixelDisplay-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/FixelDisplay-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-fixel",
+});
+
+export const metadata: Metadata = {
+  title: "Plai",
+  description: "Web-site for PLAI",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${fixel.variable} font-sans`}>
+        <Header />
+        {children}
+        <Footer/>
+      </body>
+    </html>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
+export { Button } from './button/button';
 export { Carousel } from './carousel/carousel';
 export { Contacts } from './contacts/contacts';
 export { Footer } from './footer/footer';
 export { Header } from './header/header';
-export { NotFound } from './not-found/not-found'
+export { NotFound } from './not-found/not-found';
