15f4ad2039c24fbb03b95d4435ce75c32a9dbf0c
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-11-10 15:21:35 +0200

    Removed test values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,8 @@
 import { Footer } from '@/components';
-import { Link } from '@/components/link/Link';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6">
-      <br /><hr /><br />
-
-      <Link
-        className='link-button-primary'
-        href='#'>Button-primary</Link>
-      
-      <br /><hr /><br />
-
-      <Link
-        className='link-button-secondary'
-        href='#'>Button-secondary</Link>
-
-      <br /><hr /><br />
-
-      <Link href='#'>Simple link</Link>
-
-      <br /><hr /><br />
-
-      <Link
-        className='link-text'
-        href='#'>Link-text classname</Link>
-
-      <br /><hr /><br />
       <Footer />
     </main>
   );
