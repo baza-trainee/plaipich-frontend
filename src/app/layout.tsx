389aab4dbd6be6ae79b402ce5dcfc8ce69b000ec
alexxuss1996 <alekseymelnych132@gmail.com>
2023-11-11 22:43:19 +0200

    feat: add darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import localFont from "next/font/local";
 import React from "react";
 
 import { Header } from "@/components";
+import Providers from "@/components/providers";
 
 const fixel = localFont({
   src: [
@@ -37,16 +38,14 @@ export const metadata: Metadata = {
   description: "Web-site for PLAI",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <body className={`${fixel.variable} font-sans`}>
-        <Header />
-        {children}
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
