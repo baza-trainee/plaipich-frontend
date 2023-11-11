7b93761a7e49f330489ab8016eb467d48e2db49c
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-11-11 23:03:20 +0200

    Resized image3 and add styles

--- src/components/news/News.tsx
+++ src/components/news/News.tsx
@@ -3,9 +3,10 @@ import newsData from "./news-data/newsData";
 
 export default function New() {
     return (
-        <section >
-            <h2 className='mb-10'>* Новини</h2>
-            <div className='grid grid-cols-1 lg:grid-cols-3 lg:gap-8'>
+        <section className='lg:p-16'>
+            <h2>* Новини</h2>
+            <div className='grid grid-cols-1 
+            lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16 mt-10 lg:my-[76px]'>
                 {newsData.map((news) => (
                     <NewsCard
                         key={news.id}
--- src/components/news/news-card/NewsCard.tsx
+++ src/components/news/news-card/NewsCard.tsx
@@ -22,17 +22,18 @@ const NewsCard: React.FC<NewsCardProps> = ({ newsItem, className }) => {
             <Image
                 src={imageUrl}
                 alt={title}
-                width={1280}
-                height={853} />
-            <hr className='mt-7 mb-5'/>
+                width={853}
+                height={853}
+                />
+            <hr className='border-black dark:border-white mt-7 mb-5'/>
             <div className='flex mb-5'>
-                <div className='border py-2 px-4 rounded-large bg-orange'>{tag}</div>
+                <div className='border border-black py-2 px-4 rounded-large bg-orange'>{tag}</div>
             </div>
             <h3>{title}</h3>
             <p className='py-3 '>{content}</p>
             <div className='text-gray-500' >{date}</div>
 
-            <hr className='mt-5'/>
+            <hr className='border-black dark:border-white mt-5'/>
         </article>
     );
 };
