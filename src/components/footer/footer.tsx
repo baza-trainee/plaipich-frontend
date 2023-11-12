56f73938ff99c6ae615688210de3460e15e35656
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-11-12 10:15:19 +0200

    feat: add footer markup

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,9 +1,20 @@
 export const Footer = () => {
-    return (
-        <footer>
-            <hr />
-            <div className="italic">Footer</div>
-            <hr />
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <hr />
+      <div>Logo</div>
+      <ul>
+        <li>Про нас</li>
+        <li>Новини</li>
+        <li>Проекти</li>
+        <li>Звітність</li>
+        <li>Контакти</li>
+      </ul>
+      <ul>
+        <li>Політика конфіденційності</li>
+        <li>Правила користування сайтом</li>
+      </ul>
+      <p>Розробка Baza Trainee Ukraine 2023 © Усі права захищені</p>
+    </footer>
+  );
+};
