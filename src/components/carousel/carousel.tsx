58c073a558395fdf40729e4ce9106c654f3d0afd
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-11-14 15:15:11 +0200

    feat: added all styles and fix

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -7,8 +7,6 @@ import Image from "next/image";
 import React, { useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 
-import left from "../../../public/button-arrows/left.svg";
-import right from "../../../public/button-arrows/right.svg";
 import hiro from "../../../public/hiro.png";
 import { Link } from "../link/link";
 
@@ -16,7 +14,7 @@ export const Slider = () => {
   const [slide, setSlide] = useState(1);
   const carouselList = [1, 2, 3, 4, 5];
 
-  const onChange = (page:number) => {
+  const onChange = (page: number) => {
     if (page === 0) {
       setSlide(1);
     } else {
@@ -25,7 +23,7 @@ export const Slider = () => {
   };
 
   return (
-    <section className="relative w-full py-2 bg-white" id="projects-slider">
+    <section className="relative w-full py-2 bg-white dark:bg-black" id="projects-slider">
       <Carousel
         showThumbs={false}
         showIndicators={false}
@@ -78,27 +76,37 @@ export const Slider = () => {
           </div>
         ))}
       </Carousel>
-      <div className="absolute bottom-2.5 right-1/3 w-1/3 text-dark-blue flex justify-center items-center">
-        <button className="w-20 border-none">
-          <Image
-            src={left}
-            alt={"prev"}
-            width={0}
-            height={0}
-            className="w-full object-contain"
-          />
+      <div className="absolute bottom-2.5 right-1/3 w-1/3 text-dark-blue dark:text-amber flex justify-center items-center">
+        <button className="border-none fill-dark-blue dark:fill-gray-200">
+          <svg
+            width="77"
+            height="16"
+            viewBox="0 0 77 16"
+            fill="inherit"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="inherit"
+              d="M0.292893 7.2929C-0.0976311 7.68342 -0.097631 8.31659 0.292893 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8.00001L8.07107 2.34315C8.46159 1.95263 8.46159 1.31946 8.07107 0.928938C7.68054 0.538414 7.04738 0.538414 6.65685 0.928938L0.292893 7.2929ZM77 7L1 7.00001L1 9.00001L77 9L77 7Z"
+            />
+          </svg>
         </button>
-        <p className="font-semibold text-center w-20">
+        <p className="text-md text-center w-20">
           {slide} / {carouselList.length}
         </p>
-        <button className="w-20 border-none">
-          <Image
-            src={right}
-            alt={"next"}
-            width={0}
-            height={0}
-            className="w-full object-contain"
-          />
+        <button className="w-20 border-none fill-dark-blue dark:fill-gray-200">
+          <svg
+            width="77"
+            height="16"
+            viewBox="0 0 77 16"
+            fill="inherit"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M76.7071 8.7071C77.0976 8.31658 77.0976 7.68341 76.7071 7.29289L70.3431 0.928926C69.9526 0.538402 69.3195 0.538402 68.9289 0.928926C68.5384 1.31945 68.5384 1.95262 68.9289 2.34314L74.5858 7.99999L68.9289 13.6568C68.5384 14.0474 68.5384 14.6805 68.9289 15.0711C69.3195 15.4616 69.9526 15.4616 70.3431 15.0711L76.7071 8.7071ZM8.74228e-08 9L76 8.99999L76 6.99999L-8.74228e-08 7L8.74228e-08 9Z"
+              fill="inherit"
+            />
+          </svg>
         </button>
       </div>
     </section>
