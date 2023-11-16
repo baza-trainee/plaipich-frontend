7e005694e496a74a0f149cd354873e74d6959b27
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-11-16 16:33:28 +0200

    fix:change styles

--- src/components/news-list/news-list.tsx
+++ src/components/news-list/news-list.tsx
@@ -6,15 +6,15 @@ import { Spiral } from './spiral'
 export const NewsList = () => {
     return (
         <section className='lg:p-16 p-7 bg-black text-white'>
-            <h2 className='lg:h1 h2 flex items-baseline justify-center'>
-                <Spiral/>Новини</h2>
+            <div className='flex content-center justify-center'><Spiral /><h2 className='lg:h1 h2'>
+                Новини</h2></div>
             <div className='grid grid-cols-1 
             lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16 mt-10 lg:my-[76px]'>
                 {newsData.map((news) => (
                     <NewsCard
                         key={news.id}
                         newsItem={news}
-                        className='lg:m-0 mb-10'
+                        className='lg:m-0 mb-10 hover:text-light-blue'
                     />
                 ))}
             </div>
