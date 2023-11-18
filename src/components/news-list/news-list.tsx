5e7d23699240e654a360b9bc5b1e297b471ffe54
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-11-18 17:03:24 +0200

    feat: add tablet styles

--- src/components/news-list/news-list.tsx
+++ src/components/news-list/news-list.tsx
@@ -5,11 +5,12 @@ import { Spiral } from './spiral'
 
 export const NewsList = () => {
     return (
-        <section className='lg:p-16 p-7'>
+        <section className='lg:p-16 md:px-8 md:py-16 p-7'>
             <div className='flex content-center justify-center'><Spiral /><h2 className='lg:h1 h2'>
                 Новини</h2></div>
             <div className='grid grid-cols-1 
-            lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16 mt-10 lg:my-[76px]'>
+            lg:grid-cols-3 md:gap-x-8 md:gap-y-16 mt-10 lg:my-[76px]
+            md:grid-cols-2 md:my-16'>
                 {newsData.map((news) => (
                     <NewsCard
                         key={news.id}
