39d7f3734780a53ed5fd61333a80e1fb51723300
alexxuss1996 <alekseymelnych132@gmail.com>
2023-11-21 18:28:42 +0200

    refactor: move reports into app route lng folder

--- src/components/reports/reports.tsx
+++ src/components/reports/reports.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+
+import { Link } from "@/components/link/link";
+import { accordionMockData } from "@/components/reports-accordion/accordion-data";
+import { ReportsAccordion } from "@/components/reports-accordion/reports-accordion";
 
 export const Reports = () => {
   return (
-    <div>Reports</div>
-  )
-}
+    <section className="bg-white w-full text-black pb-16">
+      <div className="max-w-desktop container w-full">
+        <h2 className="h1 text-center normal-case mb-[90px]">Звіти про діяльність</h2>
+        {accordionMockData?.map((item) => (
+          <ReportsAccordion key={item.key} title={item.title}>
+            <p className="text-2">
+              <Link appearance="link" className="text-dark-blue hover:underline" href={item.content.url}>
+                {item.content.text}
+              </Link>
+            </p>
+          </ReportsAccordion>
+        ))}
+      </div>
+    </section>
+  );
+};
