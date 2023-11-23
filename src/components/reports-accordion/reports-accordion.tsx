b2bdfccb9d736adfb98bebd4a2189143a953ab98
alexxuss1996 <alekseymelnych132@gmail.com>
2023-11-23 12:55:26 +0200

    fix: change arrow icon

--- src/components/reports-accordion/reports-accordion.tsx
+++ src/components/reports-accordion/reports-accordion.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Disclosure } from "@headlessui/react";
 import React from "react";
-import { FaArrowDownLong } from "react-icons/fa6";
+import { BsArrowDown } from "react-icons/bs";
 
 interface AccordionProps {
   title: string;
@@ -19,9 +19,8 @@ export const ReportsAccordion = ({ title, children }: AccordionProps) => {
               className={`flex transition-none h6 mb-4 last:mb-0 rounded-none w-full normal-case items-center justify-between border border-l-transparent border-r-transparent ${titleConditionalClasses} pr-4 py-6 border-gray-400 hover:text-dark-blue focus:border-l-transparent focus-border-r-transparent focus:outline-orange`}
             >
               {title}
-              <FaArrowDownLong
-                size={20}
-                strokeWidth={1}
+              <BsArrowDown
+                size={24}
                 className={
                   open
                     ? "transition-all rotate-180 transform fill-black"
