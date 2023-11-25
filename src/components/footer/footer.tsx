c8b56918b3df588763fa6cf8d964efc6fe2951ea
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-11-25 19:23:46 +0200

    feat: add tablet footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -10,7 +10,7 @@ export const Footer = async ({ lng }: { lng: string }) => {
   const { t } = await useTranslation(lng, 'footer');
   return (
     <footer className="bg-black text-white">
-      <div className="w-[320px] mx-auto lg:w-[1440px] py-[19px] lg:px-[62px] lg:pt-[40px] lg:pb-[17px]">
+      <div className="w-[320px] mx-auto md:w-[768px] lg:w-[1440px] py-[19px] lg:px-[62px] lg:pt-[40px] lg:pb-[17px]">
         <div className="lg:flex gap-[160px]">
           <Image
             src={footerSvg}
@@ -18,38 +18,40 @@ export const Footer = async ({ lng }: { lng: string }) => {
             width={674}
             height={280}
             priority
-            className="w-[265px] h-[110px] lg:w-[337px] lg:h-[140px] mb-[36px] lg:mb-0 mx-auto lg:ml-0"
+            className="w-[171px] h-[71px] md:w-[233px] md:h-[97px] lg:w-[337px] lg:h-[140px] mb-[36px] md:mb-[58px] lg:mb-0 mx-auto lg:ml-0"
           />
-          <nav>
-            <ul className="lg:flex flex-wrap lg:w-[340px] text-center lg:justify-between text-[18px] lg:text-md leading-2 lg:leading-1 lg:font-medium">
-              <li className="lg:mr-[160px] mb-[24px] lg:mb-[30px]">
-                <Link href={NAVIGATION.about}>{t('nav-about-us')}</Link>
+          <div className='md:flex md:gap-[133px]'>
+            <nav>
+              <ul className="md:flex flex-wrap md:w-[340px] text-center md:justify-between text-[18px] lg:text-md leading-2 lg:leading-1 lg:font-medium">
+                <li className="md:mr-[160px] mb-[24px] md:mb-[30px]">
+                  <Link href={NAVIGATION.about}>{t('nav-about-us')}</Link>
+                </li>
+                <li className="mb-[24px] md:mb-[30px] md:mr-[9px]">
+                  <Link href={NAVIGATION.news}>{t('nav-news')}</Link>
+                </li>
+                <li className="md:mr-[160px] mb-[24px] md:mb-[30px]">
+                  <Link href={NAVIGATION.projects}>{t('nav-projects')}</Link>
+                </li>
+                <li className="mb-[24px] md:mb-[30px]">
+                  <Link href="#">{t('nav-reporting')}</Link>
+                </li>
+                <li className="mb-[24px] md:mb-0">
+                  <Link href="#">{t('nav-contacts')}</Link>
+                </li>
+              </ul>
+            </nav>
+            <ul className="text-center md:text-start text-[18px] lg:text-md leading-3">
+              <li className="underline mb-[24px] md:mb-[22px]">
+                <Link href="#">{t('privacy-policy')}</Link>
               </li>
-              <li className="mb-[24px] lg:mb-[30px] lg:mr-[9px]">
-                <Link href={NAVIGATION.news}>{t('nav-news')}</Link>
-              </li>
-              <li className="lg:mr-[160px] mb-[24px] lg:mb-[30px]">
-                <Link href={NAVIGATION.projects}>{t('nav-projects')}</Link>
-              </li>
-              <li className="mb-[24px] lg:mb-[30px]">
-                <Link href="#">{t('nav-reporting')}</Link>
-              </li>
-              <li className="mb-[24px] lg:mb-0">
-                <Link href="#">{t('nav-contacts')}</Link>
+              <li className="underline mb-[24px] md:mb-0">
+                <Link href="#">{t('terms-of-use')}</Link>
               </li>
             </ul>
-          </nav>
-          <ul className="text-center lg:text-start text-[18px] lg:text-md leading-3">
-            <li className="underline mb-[24px] lg:mb-[22px]">
-              <Link href="#">{t('privacy-policy')}</Link>
-            </li>
-            <li className="underline mb-[24px] lg:mb-0">
-              <Link href="#">{t('terms-of-use')}</Link>
-            </li>
-          </ul>
+          </div>
         </div>
         <p className="lg:mx-[12px] mt-[52px] lg:mt-[26px] text-center lg:text-start text-base leading-3">
-        {t('footer-baza-trainee')}
+          {t('footer-baza-trainee')}
         </p>
       </div>
     </footer>
