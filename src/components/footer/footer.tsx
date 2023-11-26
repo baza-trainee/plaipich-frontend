6d1461690a95fa510aaaca0b6ed1c4a20a9312af
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-11-26 10:56:04 +0200

    fix: fix styles in footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -10,7 +10,7 @@ export const Footer = async ({ lng }: { lng: string }) => {
   const { t } = await useTranslation(lng, 'footer');
   return (
     <footer className="bg-black text-white">
-      <div className="w-[320px] mx-auto md:w-[768px] lg:w-[1440px] py-[19px] lg:px-[62px] lg:pt-[40px] lg:pb-[17px]">
+      <div className="w-[320px] mx-auto md:w-[768px] lg:w-[1440px] py-[19px] md:px-[32px] lg:px-[62px] lg:pt-[40px] lg:pb-[17px]">
         <div className="lg:flex gap-[160px]">
           <Image
             src={footerSvg}
@@ -20,19 +20,19 @@ export const Footer = async ({ lng }: { lng: string }) => {
             priority
             className="w-[171px] h-[71px] md:w-[233px] md:h-[97px] lg:w-[337px] lg:h-[140px] mb-[36px] md:mb-[58px] lg:mb-0 mx-auto lg:ml-0"
           />
-          <div className='md:flex md:gap-[133px]'>
+          <div className='md:flex md:justify-between lg:gap-[133px]'>
             <nav>
-              <ul className="md:flex flex-wrap md:w-[340px] text-center md:justify-between text-[18px] lg:text-md leading-2 lg:leading-1 lg:font-medium">
-                <li className="md:mr-[160px] mb-[24px] md:mb-[30px] hover:text-light-blue">
+              <ul className="md:flex flex-wrap md:w-[340px] text-center md:text-left md:justify-between text-[18px] lg:text-md leading-2 lg:leading-1 lg:font-medium">
+                <li className="md:w-[96px] md:mr-[140px] mb-[24px] md:mb-[30px] hover:text-light-blue">
                   <Link href={NAVIGATION.about}>{t('nav-about-us')}</Link>
                 </li>
-                <li className="mb-[24px] md:mb-[30px] md:mr-[9px] hover:text-light-blue">
+                <li className="mb-[24px] md:mb-[30px] md:w-[96px] hover:text-light-blue">
                   <Link href={NAVIGATION.news}>{t('nav-news')}</Link>
                 </li>
-                <li className="md:mr-[160px] mb-[24px] md:mb-[30px] hover:text-light-blue">
+                <li className="md:w-[96px] md:mr-[140px] mb-[24px] md:mb-[30px] hover:text-light-blue">
                   <Link href={NAVIGATION.projects}>{t('nav-projects')}</Link>
                 </li>
-                <li className="mb-[24px] md:mb-[30px] hover:text-light-blue">
+                <li className="mb-[24px] md:mb-[30px] md:w-[96px] hover:text-light-blue">
                   <Link href="#">{t('nav-reporting')}</Link>
                 </li>
                 <li className="mb-[24px] md:mb-0 hover:text-light-blue">
