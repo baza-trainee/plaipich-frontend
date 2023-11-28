0f0995cf9332e094fa92e4f4a38db139a6572643
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-11-28 15:33:26 +0200

    fix: Modified mission text design

--- src/components/mission/mission.tsx
+++ src/components/mission/mission.tsx
@@ -6,25 +6,24 @@ import missionData from "./mission-data";
 
 export const Mission = () => {
   return (
-    <section className="overflow-hidden">
-      <div
-        className="uppercase leading-3 font-bold mt-8 lg:mt-16 text-lg md:text-2xl lg:text-4xl 
-      lg:-mr-56 md:-mr-40 -mr-80 -ml-1"
-      >
-        <span className="text-gray-400">Культура</span>{" "}
-        <span className="text-gray-100">Об’єднанняЯ</span>{" "}
-        <span className="text-gray-400">Мистецтво</span>
-        <span className="text-gray-100"> Виставки </span>
-        <span className="text-gray-100">Творчість </span>
-        <span className="text-pink-pearl">Місія PLAI </span>
-        <span className="text-gray-100">Ревіталізація</span>
-        <span className="text-gray-400"> Кіно </span>
-        <span className="text-gray-100">Музика</span>
+    <section className="overflow-hidden max-w-[1440px]">
+      {/* <div className="uppercase leading-3 font-bold mt-8 lg:mt-16 text-lg md:text-2xl lg:text-4xl 
+      lg:-mr-56 md:-mr-40 -mr-80 -ml-1"></div> */}
+
+      <div className="uppercase leading-3 font-bold mt-8 lg:mt-16 text-lg md:text-2xl lg:text-4xl 
+      flex flex-wrap justify-between">
+          <span className="text-gray-400">Культура</span>{" "}
+          <span className="text-gray-100">Об’єднання</span>{" "}
+          <span className="text-gray-100 hidden md:block">Виставки</span>
+          <span className="text-gray-100 hidden md:block">Творчість</span>
+          <span className="text-gray-400">Кіно</span>
+          <span className="text-pink-pearl">Місія PLAI</span>
+          <span className="text-gray-400 hidden md:block">Мистецтво</span>
+          {/* <span className="text-gray-100">Ревіталізація</span> */}
+          <span className="text-gray-100 lg:hidden">Музика</span>
       </div>
-      <div
-        className="px-4 py-8 md:p-8 lg:p-16
-      lg:gap-8 lg:grid lg:grid-rows-none lg:grid-cols-2 lg:row-start-2"
-      >
+      <div className="px-4 py-8 md:p-8 lg:p-16
+      lg:gap-8 lg:grid lg:grid-rows-none lg:grid-cols-2 lg:row-start-2">
         <div className="md:pb-8">
           <h2 className="md:w-3/4 lg:h1 h2 normal-case">
             Наша місія сформульована наступним чином
@@ -42,7 +41,9 @@ export const Mission = () => {
                   <h3 className="text-6 md:text-5 normal-case text-light-blue mt-2">
                     {mission.title}
                   </h3>
-                  <p className="mt-1 text-3 md:text-2 lg:text-4">{mission.text}</p>
+                  <p className="mt-1 text-3 md:text-2 lg:text-4">
+                    {mission.text}
+                  </p>
                 </div>
               </div>
             ))}
@@ -52,6 +53,7 @@ export const Mission = () => {
         <Image
           src={playMissionImg}
           alt="Our mission picture"
+          loading="lazy"
           className="mx-auto lg:order-first md:block hidden"
         />
       </div>
