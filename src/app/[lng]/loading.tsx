30918a103c3dc7ecd71b94e7921e75e461d8afd4
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-12-01 13:20:07 +0200

    feat: added loader

--- src/app/[lng]/loading.tsx
+++ src/app/[lng]/loading.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+'use client'
+import Lottie from "lottie-react";
+import React from "react";
+
+import newAnime from "../../../public/animation/new-anime.json";
 
 const Loader = () => {
   return (
-    <div className='w=full h=full bg-orange'>Loader</div>
-  )
-}
+    <div className="loader w-full h-[90vh] bg-black flex justify-center items-center">
+      <Lottie animationData={newAnime} style={{width:'300px', height:'300px'}} />
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
