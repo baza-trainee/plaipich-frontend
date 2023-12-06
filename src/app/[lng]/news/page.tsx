857e1fc53ac5533804585624a21f1139b79659e9
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-12-06 19:41:53 +0200

    refactor: news list component

--- src/app/[lng]/news/page.tsx
+++ src/app/[lng]/news/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 import { useTranslation } from "@/app/i18n";
 
+import NewsList from "../../../components/news-list/news-list";
 import { Spiral } from "../../../components/spiral/spiral";
-import NewsPageList from "./news-page-list";
 
 const News = async ({
   params,
@@ -18,14 +18,14 @@ const News = async ({
     <main className="bg-white text-black ">
       <section className="py-8 px-4 flex content-center flex-col w-full max-w-[1440px] mx-auto">
         <div className="pb-8 md:pb-16 lg:pb-20 flex justify-center items-center flex-row">
-          <Spiral className="stroke-pink-pearl w-[35px] h-[27px] lg:w-[76px] lg:h-[61px] mr-3 lg:mr-4" />
+          <Spiral className="stroke-[#018ABE] w-[35px] h-[27px] lg:w-[76px] lg:h-[61px] mr-3 lg:mr-4" />
           <h1 className="h5 md:h1 lg:h1-bold">{t("title")}</h1>
         </div>
         <div className="flex flex-row justify-between">
           <div className="bg-red">bages</div>
           <div className="bg-light-blue">sort</div>
         </div>
-        <NewsPageList lng={params.lng} />
+        <NewsList lng={params.lng} />
         <div className="hidden md:flex justify-center">pagination</div>
       </section>
     </main>
