a88469ff2d4c64a33ad670816cef85a2f2edcbf5
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-12-06 17:34:20 +0200

    refactor: Spiral component

--- src/app/[lng]/news/page.tsx
+++ src/app/[lng]/news/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { useTranslation } from "@/app/i18n";
 
-import { Spiral } from "../../../components/news-list/spiral";
+import { Spiral } from "../../../components/spiral/spiral";
 import NewsPageList from "./news-page-list";
 
 const News = async ({
@@ -16,9 +16,9 @@ const News = async ({
 
   return (
     <main className="bg-white text-black ">
-      <div className="py-8 px-4 flex content-center flex-col w-full max-w-[1440px] mx-auto">
+      <section className="py-8 px-4 flex content-center flex-col w-full max-w-[1440px] mx-auto">
         <div className="pb-8 md:pb-16 lg:pb-20 flex justify-center items-center flex-row">
-          <Spiral />
+          <Spiral className="stroke-pink-pearl w-[35px] h-[27px] lg:w-[76px] lg:h-[61px] mr-3 lg:mr-4" />
           <h1 className="h5 md:h1 lg:h1-bold">{t("title")}</h1>
         </div>
         <div className="flex flex-row justify-between">
@@ -27,7 +27,7 @@ const News = async ({
         </div>
         <NewsPageList lng={params.lng} />
         <div className="hidden md:flex justify-center">pagination</div>
-      </div>
+      </section>
     </main>
   );
 };
--- src/components/news-list/news-list.tsx
+++ src/components/news-list/news-list.tsx
@@ -5,48 +5,48 @@ import { API_URL, NAVIGATION } from "@/commons/constants";
 import { useNewsList } from "@/hooks";
 
 import { Link } from "../link/link";
+import { Spiral } from "../spiral/spiral";
 import NewsCard from "./news-card/news-card";
-import { Spiral } from "./spiral";
 
 export const NewsList = ({
-    title,
-    btnText,
-    lng,
+  title,
+  btnText,
+  lng,
 }: {
-    title: string;
-    btnText: string;
-    lng: "en" | "uk";
+  title: string;
+  btnText: string;
+  lng: "en" | "uk";
 }) => {
-    const { data, isLoading } = useNewsList(API_URL.NEWS);
+  const { data, isLoading } = useNewsList(API_URL.NEWS);
 
-    return (
-        <section className="lg:p-16 md:px-8 md:py-16 p-7">
-        <div className="flex content-center justify-center">
-            <Spiral />
-            <h2 className="lg:h1 h2">{title}</h2>
-        </div>
-        {isLoading && <div className="w-full h-[350px]">Loading...</div>}
-        {!isLoading && data && (
-            <div
-            className="grid grid-cols-1 
+  return (
+    <section className="lg:p-16 md:px-8 md:py-16 p-7">
+      <div className="flex content-center justify-center">
+        <Spiral className="stroke-pink-pearl w-[35px] h-[27px] lg:w-[76px] lg:h-[61px] mr-3 lg:mr-4" />
+        <h2 className="lg:h1 h2">{title}</h2>
+      </div>
+      {isLoading && <div className="w-full h-[350px]">Loading...</div>}
+      {!isLoading && data && (
+        <div
+          className="grid grid-cols-1 
                 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16 mt-10 lg:my-[76px]
                 md:grid-cols-2 md:my-16 md:gap-x-4"
-            >
-            {data.news.map((news) => (
-                <NewsCard
-                key={news._id}
-                newsItem={news}
-                lng={lng}
-                className="lg:m-0 mb-10 hover:text-light-blue"
-                />
-            ))}
-            </div>
-        )}
-        <div className="flex justify-center">
-            <Link appearance="linkButtonPrimary" href={NAVIGATION.news}>
-            <p className="btn-text">{btnText}</p>
-            </Link>
+        >
+          {data.news.map((news) => (
+            <NewsCard
+              key={news._id}
+              newsItem={news}
+              lng={lng}
+              className="lg:m-0 mb-10 hover:text-light-blue"
+            />
+          ))}
         </div>
-        </section>
-    );
+      )}
+      <div className="flex justify-center">
+        <Link appearance="linkButtonPrimary" href={NAVIGATION.news}>
+          <p className="btn-text">{btnText}</p>
+        </Link>
+      </div>
+    </section>
+  );
 };
