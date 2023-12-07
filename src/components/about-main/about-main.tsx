9a35000e6c8b410176706afb00702113b2534f63
alexxuss1996 <alekseymelnych132@gmail.com>
2023-12-07 15:40:24 +0200

    fix: update colors

--- src/components/about-main/about-main.tsx
+++ src/components/about-main/about-main.tsx
@@ -9,15 +9,15 @@ export const AboutMain = async ({ lng }: { lng: "en" | "uk" }) => {
   const { t } = await useTranslation(lng, "about-main");
 
   return (
-    <section className="py-[72px] text-black bg-pink-pearl w-full">
-      <div className="w-full mx-auto max-w-[1440px] px-4 flex justify-normal flex-col">
+    <section className="py-[72px] text-black bg-white w-full">
+      <div className="w-full mx-auto max-w-desktop px-4 flex justify-normal flex-col">
         <div className="flex lg:flex-row flex-col justify-between items-center pb-7 md:pb-10 lg:pb-14">
           <h2 className="h1 uppercase lg:normal-case mr-5 lg:mb-0 mb-10 lg:self-auto self-start">{t("greeting")}</h2>
           <p className="leading-2 md:max-w-[950px] self-start md:text-md lg:text-xl">{t("text")}</p>
         </div>
 
         <Link
-          className="link-text self-end px-6 py-4.5 hover:no-underline transition-colors border border-transparent focus:border-black focus:outline-none hover:text-pink-pearl active:text-white rounded-large hover:bg-black"
+          className="link-text self-end px-6 py-4.5 hover:no-underline transition-colors border border-transparent focus:outline-black active:bg-gray-200 rounded-large hover:bg-gray-100"
           href={NAVIGATION.about}
         >
           {t("link")}
