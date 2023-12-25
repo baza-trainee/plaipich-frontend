c03a5097916e8c146727501cdd75f895ad114715
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-12-25 21:30:34 +0200

    fix: style

--- src/components/mission/mission.tsx
+++ src/components/mission/mission.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 
 import { useTranslation } from "@/app/i18n";
+import { Spiral } from "@/components/spiral/spiral";
 
 import playMissionImg from "../../../public/images/play-mission.jpg";
 
@@ -19,7 +20,10 @@ export const Mission = async ({ lng }: { lng: "en" | "uk" }) => {
     
   return (
     <section id="mission" className="py-8 md:py-16 md:pb-0 max-w-[1440px]">
-      <h2 className="h1 flex justify-center">{t("title")}</h2>
+      <div className="flex justify-center items-center flex-row">
+        <Spiral className="stroke-link-water w-[26px] h-[21px] md:w-[32px] md:h-[26px] lg:w-[76px] lg:h-[61px] mr-4 md:mr-6" />
+        <h2 className="h1">{t("title")}</h2>
+      </div>
       <div
         className="px-4 py-8 md:p-8 lg:p-16
       lg:gap-8 lg:grid lg:grid-rows-none lg:grid-cols-2 lg:row-start-2"
@@ -30,15 +34,12 @@ export const Mission = async ({ lng }: { lng: "en" | "uk" }) => {
             {missionData.map((mission) => (
               <div
                 key={mission.id}
-                className="border-t mt-6 md:grid md:grid-cols-[2fr,3fr]"
+                className="border-t mt-6 pt-4 md:grid md:grid-cols-[2fr,3fr]"
               >
-                <div className="text-xl font-medium mt-4 md:m-0 md:flex md:items-center ">
+                <div className="text-xl font-medium  md:flex md:items-center">
                   {mission.id}
                 </div>
                 <div>
-                  <h3 className="text-6 md:text-5 normal-case text-light-blue mt-2">
-                    {mission.title}
-                  </h3>
                   <p className="mt-1 text-3 md:text-2 lg:text-4">
                     {mission.text}
                   </p>
