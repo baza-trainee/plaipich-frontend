817e21e6555306b3a030ae768fb8282965547b11
NaTaLiYa01 <nmoskalenko911@gmail.com>
2024-01-17 20:37:00 +0200

    feat: creates project marckup

--- src/app/[lng]/projects/page.tsx
+++ src/app/[lng]/projects/page.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image';
 import React from 'react';
 
 import { useTranslation } from '@/app/i18n';
 import { API_URL } from '@/commons/constants';
 import { IProject } from '@/commons/types';
+import { Button } from '@/components/button/button';
+import { Spiral } from "@/components/spiral/spiral";
 import { apiService } from '@/services/api-service';
 
 const Projects = async ({
@@ -17,6 +20,8 @@ const Projects = async ({
     data: { projects },
   }: { data: { projects: IProject[] } } = await apiService.getRequest(API_URL.PROJECTS);
 
+  // console.log(Projects);
+
   return (
     <main>
       <p className="mt-[32px] text-6 mb-12 md:mb-[100px] w-full max-w-[1440px] mx-auto px-4 md:px-8">
@@ -25,9 +30,20 @@ const Projects = async ({
       <h1 className="text-center lg:mb-[120px] text-small-4xl leading-2 lg:text-4xl lg:leading-4 max-w-[1440px] text-white">
         {t('title')}
       </h1>
-      {projects.map((item) => (
-        <div key={item._id}>{item.title}</div>
-      ))}
+      <div className="flex flex-wrap gap-[29px]">
+        {projects.map((item) => (
+          <div key={item._id} className="w-[645px]">
+            <Image alt={item.title} src={item.poster} width={645} height={411} />
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+            <Spiral className="stroke-water-blue w-[35px] h-[27px] lg:w-[40px] lg:h-[32px] mr-3 lg:mr-4" />
+            <p>потребує підтримки</p>
+            <Button type='button' className=''>Дізнатись більше</Button>
+            {/* <h2>{item.enTitle}</h2>
+          <p>{item.enDescription}</p> */}
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
