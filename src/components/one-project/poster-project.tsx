123429c676db8f42bbe6e0cd80c4c37317fdd24e
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2024-02-16 11:05:21 +0200

    fix: styles for poster

--- src/components/one-project/poster-project.tsx
+++ src/components/one-project/poster-project.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import React from "react";
 
+import { NAVIGATION } from "@/commons/constants";
+
 import { Button } from "..";
 
 export const Poster = ({
@@ -17,7 +19,7 @@ export const Poster = ({
   return (
     <section
       id="poster"
-      className="max-w-[1440px] mx-auto text-white md:flex md:flex-row-reverse p-4 md:p-8"
+      className="max-w-[1440px] mx-auto text-white md:flex md:flex-row-reverse p-4 md:p-8 lg:px-16"
     >
       <div className="w-full md:w-1/2 h-[400px] lg:w-[640px] lg:h-[640px] overflow-hidden">
         <Image
@@ -28,8 +30,21 @@ export const Poster = ({
           className="w-full h-full object-cover object-center"
         />
       </div>
-      <div className="flex-1 flex flex-col justify-center items-start pt-4 md:p-12 gap-16">
-        <h1 className="text-small-3xl text-link-water text-center md:text-left lg:text-4xl lg:leading-4 font-bold max-w-[450px] uppercase">
+      <div className="flex-1 flex flex-col justify-center items-start pt-4 md:py-12 gap-16 lg:pb-16 lg:pt-0 lg:flex-1">
+        <ul className="hidden lg:flex text-sm gap-2">
+          <li>
+            <a href={`/${lng}${NAVIGATION.main}`}>
+              {lng === "en" ? "Main / " : "Головна / "}
+            </a>
+          </li>
+          <li>
+            <a href={`/${lng}${NAVIGATION.projects}`}>
+              {lng === "en" ? "Projects / " : "Проєкти / "}
+            </a>
+          </li>
+          <li className="text-light-blue text-sm cursor-pointer">{title}</li>
+        </ul>
+        <h1 className="flex-1 items-center flex text-small-3xl text-link-water text-center md:text-left lg:text-4xl lg:leading-4 font-bold max-w-[450px] uppercase">
           {title}
         </h1>
         {!status && (
