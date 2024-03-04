37a9bd8b31f28626bc1d205ada742ee4d457d942
alexxuss1996 <alekseymelnych132@gmail.com>
2024-03-04 20:48:09 +0200

    feat: gallery layout

--- src/components/one-project/one-project.tsx
+++ src/components/one-project/one-project.tsx
@@ -54,7 +54,13 @@ export const OneProject = ({ lng }: { lng: "en" | "uk" }) => {
               logo={data.support.logo}
             />
           )}
-          {data.photos && data.photos.length > 0 && <Gallery />}
+          {data.photos && data.photos.length > 0 && (
+            <Gallery
+              title={lng === "en" ? "Gallery" : "Галерея"}
+              photos={data.photos}
+              videos={data.videos}
+            />
+          )}
         </>
       ) : (
         <Loader />
