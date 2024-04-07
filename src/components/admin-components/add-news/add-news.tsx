17aa58a32429f971f5b3938746beb7ca0096535a
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2024-04-07 15:30:41 +0300

    feat: added upload images

--- src/components/admin-components/add-news/add-news.tsx
+++ src/components/admin-components/add-news/add-news.tsx
@@ -1,151 +1,220 @@
-'use client'
-import { useForm } from "react-hook-form"
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import { upload } from "@/utils";
 
 export type FormData = {
-    title: string
-    titleEn: string
-    titleImg: string
-    lid: string
-    lidEn: string
-    text: string
-    textEn: string
-    media: string
-    category: string
-    date: string
-}
+  title: string;
+  titleEn: string;
+  titleImg: string;
+  lid: string;
+  lidEn: string;
+  text: string;
+  textEn: string;
+  media: string;
+  category: string;
+  date: string;
+};
 
 export const AddNews = ({ className }: Readonly<{ className?: string }>) => {
+  const [images, setImages] = useState<string[]>([]);
 
-    function onSubmit(data: FormData) {
-        console.log(data);
-        // sendEmail(data);
-    }
+  const addImages = (event: any) => {
+    upload(event?.target.files[0]).then((data) =>
+      setImages((prev) => [...prev, data])
+    );
+  };
 
-    const {
-        register,
-        handleSubmit,
-    } = useForm<FormData>()
+  function onSubmit(data: FormData) {
+    console.log(data);
+    // sendEmail(data);
+  }
 
-    return (
-        <section className={` p-4 bg-gray-200 text-black ${className ?? ''}`}>
-            <div className="p-[1em] bg-white mb-[1em] flex justify-between">
-                <h1 className="h7 ">Додати новий запис</h1>
-                <button
-                    className="font-semibold px-[2em] py-[0.5em] bg-dark-blue text-white "
-                    type="submit">Опублікувати</button>
+  const { register, handleSubmit } = useForm<FormData>();
+
+  return (
+    <section className={` p-4 bg-gray-200 text-black ${className ?? ""}`}>
+      <div className="p-[1em] bg-white mb-[1em] flex justify-between">
+        <h1 className="h7 ">Додати новий запис</h1>
+        <button
+          className="font-semibold px-[2em] py-[0.5em] bg-dark-blue text-white "
+          type="submit"
+        >
+          Опублікувати
+        </button>
+      </div>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col ">
+        <div className="grid grid-cols-3 gap-4 mb-[1em] ">
+          <div className="rounded-lg p-[1em] bg-white flex flex-col justify-between">
+            <p className="text-5">Категорія</p>
+            <label className="text-6 ">
+              Обрати категорію{" "}
+              <select
+                placeholder="Обирати зі списку"
+                {...register("category", { required: true })}
+                className="rounded-sm border-2 p-2"
+              >
+                <option disabled value="alien">
+                  Обирати зі списку
+                </option>
+                <option value="alien">alien</option>
+                <option value="human">human</option>
+                <option value="cat">cat</option>
+                <option value="dog">dog</option>
+                <option value="other">other</option>
+              </select>
+            </label>
+          </div>
+
+          <div className="rounded-lg bg-white p-[1em] flex flex-col justify-between ">
+            <label className="text-6 ">Дата публікації </label>
+            <input
+              placeholder="{date}"
+              type="date"
+              defaultValue={new Date().toISOString().substring(0, 10)}
+              {...register("date", { required: false })}
+              className="rounded-sm border-2 p-2"
+            />
+          </div>
+
+          <div className="rounded-lg bg-white p-[1em] flex flex-col justify-between">
+            <p className="text-5 ">Запис</p>
+            <div className="flex justify-between">
+              <button
+                type="submit"
+                className=" font-semibold px-[1em] py-[0.5em] border border-black"
+              >
+                Переглянути
+              </button>
+              <button
+                type="submit"
+                className=" font-semibold px-[1em] py-[0.5em] "
+              >
+                Зберегти
+              </button>
             </div>
+          </div>
+        </div>
 
-            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col ">
+        <div className="mb-[1em] rounded-lg bg-white p-[1em] grid grid-cols-2 gap-4">
+          <h3 className="h15">Українська версія запису</h3>
+          <h3 className="h15">Англійська версія запису</h3>
 
-                <div className="grid grid-cols-3 gap-4 mb-[1em] ">
-                    <div className="rounded-lg p-[1em] bg-white flex flex-col justify-between">
-                        <p className="text-5">Категорія</p>
-                        <label className="text-6 ">Обрати категорію{' '}
-                            <select
-                                placeholder="Обирати зі списку"
-                                {...register("category", { required: true })}
-                                className="rounded-sm border-2 p-2" >
-                                <option disabled value="alien">Обирати зі списку</option>
-                                <option value="alien">alien</option>
-                                <option value="human">human</option>
-                                <option value="cat">cat</option>
-                                <option value="dog">dog</option>
-                                <option value="other">other</option>
-                            </select>
-                        </label>
-                    </div>
+          <label className="text-6 ">
+            Додати заголовок*{" "}
+            <input
+              placeholder="Введіть заголовок"
+              type="text"
+              {...register("title", { required: true, maxLength: 80 })}
+              className="rounded-sm border-2 p-2 "
+            />
+          </label>
+          <label className="text-6 ">
+            Додати заголовок англійською мовою*{" "}
+            <input
+              placeholder="Введіть заголовок"
+              type="text"
+              {...register("titleEn", { required: true, maxLength: 80 })}
+              className="rounded-sm border-2 p-2 "
+            />
+          </label>
 
-                    <div className="rounded-lg bg-white p-[1em] flex flex-col justify-between ">
-                        <label className="text-6 ">Дата публікації{' '}
-                        </label>
-                        <input placeholder="{date}"
-                            type="date"
-                            defaultValue={new Date().toISOString().substring(0, 10)}
-                            {...register("date", { required: false })}
-                            className="rounded-sm border-2 p-2" />
-                    </div>
-
-                    <div className="rounded-lg bg-white p-[1em] flex flex-col justify-between">
-                        <p className="text-5 ">Запис</p>
-                        <div className="flex justify-between">
-                            <button type="submit" className=" font-semibold px-[1em] py-[0.5em] border border-black">Переглянути</button>
-                            <button type="submit" className=" font-semibold px-[1em] py-[0.5em] ">Зберегти</button>
-                        </div>
-                    </div>
+          <div className="col-span-2">
+            <p className="text-6 ">Додати зображення*</p>
+            {images[0] ? (
+              <>
+                <Image
+                  src={images[0]}
+                  alt="poster"
+                  width={1000}
+                  height={500}
+                  className="w-full h-48 border-2 border-dashed rounded-lg object-contain"
+                />{" "}
+              </>
+            ) : (
+              <label className="flex flex-col items-center justify-center w-full h-48 border-2 border-dashed rounded-lg cursor-pointer">
+                <div className="flex flex-col items-center justify-center text-dark-blue">
+                  <span className="-mb-[0.5em] text-[4em] font-bold">+</span>
+                  <p>Додати зображення</p>
                 </div>
+                <input type="file" className="hidden" onChange={addImages} />
+              </label>
+            )}
+          </div>
 
-                <div className="mb-[1em] rounded-lg bg-white p-[1em] grid grid-cols-2 gap-4">
+          <label className="text-6 ">
+            Додати лід до публікації*{" "}
+            <textarea
+              rows={3}
+              {...register("lid", { required: true })}
+              className="rounded-sm border-2 p-2 "
+            />
+          </label>
+          <label className="text-6 ">
+            Додати лід до публікації англійською мовою*{" "}
+            <textarea
+              rows={3}
+              {...register("lidEn", { required: true })}
+              className="rounded-sm border-2 p-2 "
+            />
+          </label>
 
-                    <h3 className="h15">Українська версія запису</h3>
-                    <h3 className="h15">Англійська версія запису</h3>
+          <p className="px-32 col-span-2 text-red  text-sm">
+            Лід виконує функцію першого абзацу. Лід завжди виділений жирним
+            шрифтом. Також, він виконує функцію анонсу статті (короткий зміст)
+            на головній сторінці
+          </p>
 
-                    <label className="text-6 ">Додати заголовок*{' '}
-                        <input placeholder="Введіть заголовок"
-                            type="text"
-                            {...register("title", { required: true, maxLength: 80 })}
-                            className="rounded-sm border-2 p-2 " />
-                    </label>
-                    <label className="text-6 ">Додати заголовок англійською мовою*{' '}
-                        <input placeholder="Введіть заголовок"
-                            type="text"
-                            {...register("titleEn", { required: true, maxLength: 80 })}
-                            className="rounded-sm border-2 p-2 " />
-                    </label>
+          <label className="text-6  ">
+            Додати текст до публікації*{" "}
+            <textarea
+              rows={4}
+              {...register("text", { required: true })}
+              className="rounded-sm border-2 p-2"
+            />
+          </label>
+          <label className="text-6  ">
+            Додати текст до публікації англійською мовою*{" "}
+            <textarea
+              rows={4}
+              {...register("textEn", { required: true })}
+              className="rounded-sm border-2 p-2"
+            />
+          </label>
 
-                    <div className="col-span-2">
-                        <p className="text-6 ">Додати зображення*</p>
-                        <label className="flex flex-col items-center justify-center w-full h-48 border-2 border-dashed rounded-lg cursor-pointer">
-                            <div className="flex flex-col items-center justify-center text-dark-blue">
-                                <span className="-mb-[0.5em] text-[4em] font-bold">+</span>
-                                <p>Додати зображення</p>
-                            </div>
-                            <input type="file" className="hidden" {...register("titleImg", { required: true })} />
-                        </label>
-                    </div>
-
-                    <label className="text-6 ">Додати лід до публікації*{' '}
-                        <textarea
-                            rows={3}
-                            {...register("lid", { required: true })}
-                            className="rounded-sm border-2 p-2 " />
-                    </label>
-                    <label className="text-6 ">Додати лід до публікації англійською мовою*{' '}
-                        <textarea
-                            rows={3}
-                            {...register("lidEn", { required: true })}
-                            className="rounded-sm border-2 p-2 " />
-                    </label>
-
-                    <p className="px-32 col-span-2 text-black/75 text-sm">
-                        <span className="text-red font-extrabold">Червоний текст серед сторінки 1й раз зверне на себе увагу, потім - буде дратувати і поступово виїдати очи тому, хто за постійно додає новини. Для збільшення єфекту пропоную зробити ще й жирним :-/</span> Лід виконує функцію першого абзацу. Лід завжди виділений жирним шрифтом. Також, він виконує функцію анонсу статті (короткий зміст) на головній сторінці</p>
-
-                    <label className="text-6  ">Додати текст до публікації*{' '}
-                        <textarea
-                            rows={4}
-                            {...register("text", { required: true })}
-                            className="rounded-sm border-2 p-2" />
-                    </label>
-                    <label className="text-6  ">Додати текст до публікації англійською мовою*{' '}
-                        <textarea
-                            rows={4}
-                            {...register("textEn", { required: true })}
-                            className="rounded-sm border-2 p-2" />
-                    </label>
-
-                    <div className="col-span-2">
-                        <p className="text-6 ">Додати медіа</p>
-                        <label className="flex flex-col items-center justify-center w-full h-48 border-2 border-dashed rounded-lg cursor-pointer">
-                            <div className="flex flex-col items-center justify-center text-dark-blue">
-                                <span className="-mb-[0.5em] text-[4em] font-bold">+</span>
-                                <p>Додати зображення</p>
-                            </div>
-                            <input type="file" className="hidden" {...register("media", { required: false })} />
-                        </label>
-                    </div>
-
-                </div>
-
-            </form>
-        </section>
-    )
-}
+          <div className="col-span-2">
+            <p className="text-6 ">Додати зображення*</p>
+            {images.length > 1 && (
+              <>
+                {images.map((image, ind) => {
+                  if (ind !== 0) {
+                    return (
+                      <Image
+                        src={images[ind]}
+                        key={image}
+                        alt="poster"
+                        width={1000}
+                        height={500}
+                        className="w-full h-48 border-2 border-dashed rounded-lg object-contain"
+                      />
+                    );
+                  }
+                })}
+              </>
+            )}
+            <label className="flex flex-col items-center justify-center w-full h-48 border-2 border-dashed rounded-lg cursor-pointer">
+              <div className="flex flex-col items-center justify-center text-dark-blue">
+                <span className="-mb-[0.5em] text-[4em] font-bold">+</span>
+                <p>Додати зображення</p>
+              </div>
+              <input type="file" className="hidden" onChange={addImages} />
+            </label>
+          </div>
+        </div>
+      </form>
+    </section>
+  );
+};
